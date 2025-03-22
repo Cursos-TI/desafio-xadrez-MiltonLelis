b4a7d984288d74c4deb95224814cfe98d7cde47c
MiltonLelis <135756305+MiltonLelis@users.noreply.github.com>
2025-03-22 21:00:55 +0000

    Desafio Xadrez - Nível Mestre - Completo

--- xadrez.c
+++ xadrez.c
@@ -7,7 +7,14 @@
 // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 void moverBispo(int casas) {
     if (casas > 0) {
-        printf("Cima Direita\n");
+        // Demonstrando o movimento do Bispo com loops aninhados
+        for (int i = 0; i < 1; i++) { // movimento vertical
+            printf("Cima ");
+            for (int j = 0; j < 1; j++) { // movimento horizontal
+                printf("Direita");
+            }
+            printf("\n");
+        }
         moverBispo(casas - 1);
     }
 }
@@ -31,12 +38,13 @@ void moverRainha(int casas) {
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int i, j;  // variáveis de controle de loops
     int numeroDeCasasBispo = 5;
     int numeroDeCasasTorre = 5;
     int numeroDeCasasRainha = 8;
-    int numeroDeCasasCavaloParaCima = 2;
-    int numeroDeCasasCavaloParaDireita = 1;
+    int numeroDeCasasCavaloVertical = 2; // Quantidade de Casas que o Cavalo se moverá na Vertical
+    int movimentoDoCavaloNaVertical = 1; // Tipo de Movimento na Vertical: 1 - Cima; 2 - Baixo 
+    int numeroDeCasasCavaloHorizontal = 1; // Quantidade de Casas que o Cavalo se moverá na Horizontal
+    int movimentoDoCavaloNaHorizontal = 2; // Tipo de Movimento na Horizontal: 1 - Esquerda; 2 - Direita
 
     // Implementação de Movimentação do Bispo utilizando a estrutura while
     // Movimento do Bispo cinco casas na diagonal para cima e à direita
@@ -68,14 +76,23 @@ int main() {
     // Movimento do Cavalo duas casas para cima e uma para a direita
     // Implementando a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Demonstrando o uso de continue e break dentro do loop.
-    for (i = 1, j = 1; i <= numeroDeCasasCavaloParaCima && j <= numeroDeCasasCavaloParaDireita; i++)
+    for (int i = 1, j = 1; i <= numeroDeCasasCavaloVertical && j <= numeroDeCasasCavaloHorizontal; i++)
     {
-        printf("Cima\n");
+        if (movimentoDoCavaloNaVertical == 1) {
+            printf("Cima\n");
+        } else if (movimentoDoCavaloNaVertical == 2) {
+            printf("Baixo\n");
+        }
+
         if (i % 2 != 0) {
             continue;
         }
-        printf("Direita\n");
-        break;
+
+        if (movimentoDoCavaloNaHorizontal == 1) {
+            printf("Esquerda\n");
+        } else if (movimentoDoCavaloNaHorizontal == 2) {
+            printf("Direita\n");
+        }
     }
     printf("\n");
 
